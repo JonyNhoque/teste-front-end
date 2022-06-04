02654783f30f15f8d62ea2c2c885816eeddd84b4
JonyNhoque <jgabriel.nhoque@gmail.com>
2022-06-04 16:32:56 -0300

    feat: sass setup. topHeader component created. SVG import setup with custom types.

---
+++ src/components/header/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import MainHeader from './MainHeader';
+import TopHeader from './topHeader';
+
+const Header = () => {
+  return (
+    <>
+      <TopHeader />
+      {/* <MainHeader /> */}
+    </>
+  );
+};
+
+export default Header;
